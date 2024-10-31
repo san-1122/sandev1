a1e0d89152e4ae130c5522c50c84ede76d9fef2b
sanhathai <sanhathai.microdevops@gmail.com>
2024-10-31 23:50:40 +0700

    ครั้งแรก

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const config = {
     channelSecret: 'ec5dd65b9cb56ed5b24a0a54ef9a94fb'
 };
 
+// สร้างตัวแปรวันที่ปัจจุบัน
+const currentDate = new Date();
+
+// แยกวัน เดือน ปี
+const day = currentDate.getDate();         // วันที่ (1 - 31)
+const month = currentDate.getMonth() + 1;  // เดือน (1 - 12), +1 เพราะ getMonth() เริ่มจาก 0
+const year = currentDate.getFullYear();    // ปี (เช่น 2024)
+const datetime = `${day}/${month}/${year}`;
 const client = new line.Client(config);
 
 // Middleware สำหรับ parsing body ของ request
@@ -37,19 +45,38 @@ function handleEvent(event) {
     return Promise.resolve(null);
 }
 
-cron.schedule('49 22 * * *', () => {
-    const message = 'นี่คือข้อความแจ้งเตือน\nตามเวลาที่กำหนด (10:26 น.)';
+function broadcastScheduledMessage(message) {
+    return axios.post(
+        'https://api.line.me/v2/bot/message/broadcast',
+        {
+            messages: [
+                {
+                    type: 'text',
+                    text: message,
+                },
+            ],
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${config.channelAccessToken}`,
+                'Content-Type': 'application/json',
+            },
+        }
+    );
+}
+
+cron.schedule('42 23 * * *', () => {
+    const datetime2 = ` ${datetime}/18.00น.`;
+    const hyung = ` \nของ พี่หยง \n156.232.105.0/25 (vlan 565) \nbandwith : ปกติ(251M) \n156.232.105.128/25 (vlan 568) \n\n`;
+    const nikky = ` ของ นิกกี้ \n156.232.106.0/25 (vlan 566) \nbandwith : ปกติ(432M) \n154.209.146.0/25 (vlan 567) \nbandwith : ปกติ(432M)\n156.232.106.128/25 (vlan 569)\nbandwith : ปกติ(432M)\n154.209.146.128/25 (vlan 570)\nbandwith : ปกติ(432M)\n\n`;
+    const sumBW = `รวม BW User = 1.74 G`;
+    const message = datetime2 + hyung + nikky + sumBW;
     broadcastScheduledMessage(message)
         .then(() => console.log('ส่งข้อความ Broadcast สำเร็จที่เวลา 10:26 น.'))
         .catch((error) => console.error('เกิดข้อผิดพลาด:', error));
 });
 
-cron.schedule('50 22 * * *', () => {
-    const message = 'ของ พี่หยง\nตามเวลาที่กำหนด (10:35 น.)';
-    broadcastScheduledMessage(message)
-        .then(() => console.log('ส่งข้อความ Broadcast สำเร็จที่เวลา 10:35 น.'))
-        .catch((error) => console.error('เกิดข้อผิดพลาด:', error));
-});
+
 // เริ่มต้นเซิร์ฟเวอร์
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
